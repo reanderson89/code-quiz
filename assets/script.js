7b530e98b859ba40b628dbb1083cb5cd3c8eee6b
Robert Anderson <reanderson89@gmail.com>
2020-09-27 00:41:45 -0400

    added a reset.css

--- assets/script.js
+++ assets/script.js
@@ -4,5 +4,22 @@ var body = document.querySelectorAll("body");
 // h1.textContent = "Hello World";
 // document.body.appendChild(h1);
 
+// main screen has a "start quiz" button with some writing above it. 
+
+// The timer in the top right is set to 0 until start quiz is clicked then it is set to 75.
+
+// There is a "View Highscores" button in the top left that when clicked lists out all of the scores. (maybe top 5).
+
+// on click of start quiz the screen changes to a question and the timer begins to countdown from 75 seconds. 
+
+// on click of an answer the screen changes again, on the next screen text flashes underneath the answers telling you if you are right or wrong.
+
+// If you are wrong then you lose 15 seconds from your time.
+
+// If you finish the quiz before time runs out then your score is total time remaining.
+
+// if time reaches 0 before you finish all of the questions, your score would be 0. Better luck next time.
+
+// When you get your score you input your name and submit it, the screen then changes to the highscores page where you can see your score.
 
 
